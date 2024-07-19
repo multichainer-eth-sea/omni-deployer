fc845cd480c3400ad552d4712bf1d49327c210e7
Dimas <dimasriat@gmail.com>
2024-07-20 01:23:36 +0700

    feat(cli): add aptos example

--- packages/cli-example/src/config/config.ts
+++ packages/cli-example/src/config/config.ts
@@ -3,7 +3,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const ARBITRUM_PRIVATE_KEY = process.env.ARBITRUM_PRIVATE_KEY || '';
+const APTOS_PRIVATE_KEY = process.env.APTOS_PRIVATE_KEY || '';
 
 export const config = {
   ARBITRUM_PRIVATE_KEY,
+  APTOS_PRIVATE_KEY,
 };
