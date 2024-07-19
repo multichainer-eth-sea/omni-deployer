cafe75d7384116c104a1b9b845ad98ab832342ec
Dimas <dimasriat@gmail.com>
2024-07-20 00:18:21 +0700

    feat(cli): add common

--- packages/cli-example/src/common/common.ts
+++ packages/cli-example/src/common/common.ts
@@ -6,11 +6,41 @@ import {
   Sdk,
   SolClientAdapter,
 } from '@omni-deployer/sdk';
+import { ethers } from 'ethers';
+import { config } from '../config';
+
+export function prepareEvmClient(
+  rpcUrl: string,
+  privateKey: string,
+  blockExplorerUrl: string,
+) {
+  const provider = new ethers.JsonRpcProvider(rpcUrl);
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  return new EvmClientAdapter({
+    ethersProvider: provider,
+    ethersSigner: signer,
+    networkMetadata: {
+      gasTicker: 'ETH',
+      blockExplorerUrl: blockExplorerUrl,
+      gasDecimals: 18,
+      gasPriceCoingeckoId: 'ethereum',
+    },
+  });
+}
 
 export function prepareSdk() {
   const clientMap: ClientMap = {
-    [CoinChain.ARBITRUM]: new EvmClientAdapter(),
-    [CoinChain.OPTIMISM]: new EvmClientAdapter(),
+    [CoinChain.ARBITRUM]: prepareEvmClient(
+      'https://arbitrum.llamarpc.com',
+      config.ARBITRUM_PRIVATE_KEY,
+      'https://arbiscan.io/',
+    ),
+    [CoinChain.OPTIMISM]: prepareEvmClient(
+      'https://optimism.llamarpc.com',
+      config.ARBITRUM_PRIVATE_KEY,
+      'https://optimistic.etherscan.io/',
+    ),
     [CoinChain.SOLANA]: new SolClientAdapter(),
     [CoinChain.APTOS]: new AptClientAdapter(),
   };
