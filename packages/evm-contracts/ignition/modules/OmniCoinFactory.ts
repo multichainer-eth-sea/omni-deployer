bf28c86304ca1dc4a47adb76f797c0f1be5edceb
Dimas <dimasriat@gmail.com>
2024-07-25 21:39:50 +0700

    chore(evm): remove ignition

--- packages/evm-contracts/ignition/modules/OmniCoinFactory.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-
-const OmniCoinFactoryModule = buildModule('OmniCoinFactory', (m) => {
-  const omniCoinFactory = m.contract('OmniCoinFactory');
-
-  return { omniCoinFactory };
-});
-
-export default OmniCoinFactoryModule;
