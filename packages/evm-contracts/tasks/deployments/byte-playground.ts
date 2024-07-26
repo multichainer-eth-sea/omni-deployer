764309d7da1cd9b1ab6e9feac935194dfb20df29
Dimas <dimasriat@gmail.com>
2024-07-27 02:26:42 +0700

    feat(evm): add byte playground

---
+++ packages/evm-contracts/tasks/deployments/byte-playground.ts
@@ -0,0 +1,23 @@
+import { scope } from 'hardhat/config';
+
+const deployTask = scope('deploy');
+
+deployTask
+  .task('byte-playground', 'Deploys the BytePlayground contract')
+  .setAction(async (_, hre) => {
+    const ContractInit = await hre.ethers.getContractFactory('BytePlayground');
+    const contract = await ContractInit.deploy();
+    await contract.waitForDeployment();
+    const deployedAddress = await contract.getAddress();
+
+    console.log(`ContractInit deployed at ${deployedAddress}`);
+
+    await hre.run('verify:verify', {
+      address: deployedAddress,
+      constructorArguments: [],
+    });
+
+    console.log(`ContractInit verified`);
+  });
+
+// npx hardhat deploy omnicoin --name "OmniCoin" --symbol "OMC" --decimals 18 --total-supply 1000000000000000000000000000 --receiver "0x976922801d71035C17967F2FEE7E137503aea6C0" --network optimisticEthereum
--- packages/evm-contracts/tasks/deployments/index.ts
+++ packages/evm-contracts/tasks/deployments/index.ts
@@ -1,3 +1,5 @@
 export * from './omnicoin';
 export * from './lz-v1';
 export * from './omnicoin-factory';
+
+export * from './byte-playground';
