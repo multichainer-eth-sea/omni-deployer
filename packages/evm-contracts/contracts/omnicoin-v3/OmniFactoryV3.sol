2bff7dc0b9621e1cfe0e265ab248112b85ad4b47
Dimas <dimasriat@gmail.com>
2024-07-28 02:39:31 +0700

    feat(evm): add deploy remote coin

--- packages/evm-contracts/contracts/omnicoin-v3/OmniFactoryV3.sol
+++ packages/evm-contracts/contracts/omnicoin-v3/OmniFactoryV3.sol
@@ -5,19 +5,29 @@ import { ILayerZeroReceiver } from '@layerzerolabs/solidity-examples/contracts/l
 import { NonblockingLzApp } from '@layerzerolabs/solidity-examples/contracts/lzApp/NonblockingLzApp.sol';
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OmniCoinV3 } from "./OmniCoinV3.sol";
+// import "hardhat/console.sol";
 
 contract OmniFactoryV3 is NonblockingLzApp {
     event LocalCoinDeployed(address indexed coinAddress, address indexed coinReceiver);
+    event RemoteCoinDeployed(address indexed remoteFactoryAddress, address indexed creator, uint16 indexed chainId);
 
     constructor(address _endpoint) NonblockingLzApp(_endpoint) Ownable(msg.sender)  {}
 
     function _nonblockingLzReceive(
-        uint16 _srcChainId,
+        uint16,
         bytes memory,
         uint64,
         bytes memory _payload
     ) internal override {
-        // ...
+        (
+            string memory coinName,
+            string memory coinTicker,
+            uint8 coinDecimals,
+            uint256 coinTotalSupply,
+            address receiver
+        ) = abi.decode(_payload, (string, string, uint8, uint256, address));
+
+        _deployLocalCoin(coinName, coinTicker, coinDecimals, coinTotalSupply, receiver);
     }
 
     function deployLocalCoin(
@@ -25,14 +35,83 @@ contract OmniFactoryV3 is NonblockingLzApp {
         string memory coinTicker,
         uint8 coinDecimals,
         uint256 coinTotalSupply
-    ) external {
+    ) public {
+        _deployLocalCoin(coinName, coinTicker, coinDecimals, coinTotalSupply, msg.sender);
+    }
+
+    function _deployLocalCoin(
+        string memory coinName,
+        string memory coinTicker,
+        uint8 coinDecimals,
+        uint256 coinTotalSupply,
+        address receiver
+    ) internal {
         OmniCoinV3 newCoin = new OmniCoinV3(
             coinName,
             coinTicker,
             coinDecimals,
             coinTotalSupply,
-            msg.sender
+            receiver
         );
-        emit LocalCoinDeployed(address(newCoin), msg.sender);
+        emit LocalCoinDeployed(address(newCoin), receiver);
     }
+
+    function estimateFee(
+        string memory coinName,
+        string memory coinTicker,
+        uint8 coinDecimals,
+        uint256 coinTotalSupply,
+        address receiver,
+        uint16 remoteChainId
+    ) public view returns (uint nativeFee, uint zroFee) {
+        uint16 version = 1;
+        uint256 gasForDestinationLzReceive = 3500000;
+        bytes memory adapterParams = abi.encodePacked(version, gasForDestinationLzReceive);
+
+        bytes memory payload = abi.encode(
+            coinName, 
+            coinTicker, 
+            coinDecimals, 
+            coinTotalSupply,
+            receiver
+        );
+        return lzEndpoint.estimateFees(remoteChainId, address(this), payload, false, adapterParams);
+    }
+
+    function deployRemoteCoin(
+        string memory coinName,
+        string memory coinTicker,
+        uint8 coinDecimals,
+        uint256 coinTotalSupply,
+        address receiver,
+        uint16 remoteChainId,
+        address remoteChainAddress
+    ) public payable {
+        bytes memory payload = abi.encode(
+            coinName, 
+            coinTicker, 
+            coinDecimals, 
+            coinTotalSupply,
+            receiver
+        );
+
+        uint16 version = 1;
+        uint256 gasForDestinationLzReceive = 3500000;
+        bytes memory adapterParams = abi.encodePacked(version, gasForDestinationLzReceive);
+
+        _lzSend(
+            remoteChainId,
+            payload,
+            payable(msg.sender),
+            address(0x0),
+            adapterParams,
+            address(this).balance
+        );
+
+        emit RemoteCoinDeployed(remoteChainAddress, msg.sender, remoteChainId);
+    }
+
+    // allow this contract to receive ether
+    receive() external payable {}
+
 }
--- packages/evm-contracts/test/OmniFactoryV3.ts
+++ packages/evm-contracts/test/OmniFactoryV3.ts
@@ -1,17 +1,19 @@
 import { expect } from 'chai';
 import hre from 'hardhat';
 
-describe('OmniFactoryV3',  () => {
-  describe('(deployment)',  () => {
+describe('OmniFactoryV3', () => {
+  describe('(deployment)', () => {
     it('should set the right layerzero endpoint', async () => {
       // ---------- arrange ---------- //
       // prepare the endpoints
-      const LZEndpointMock = await hre.ethers.getContractFactory('LZEndpointMock');
+      const LZEndpointMock =
+        await hre.ethers.getContractFactory('LZEndpointMock');
       const lzEndpoint = await LZEndpointMock.deploy(1);
       const lzEndpointAddress = await lzEndpoint.getAddress();
-    
+
       // deploy the factory contract
-      const OmniFactoryV3 = await hre.ethers.getContractFactory('OmniFactoryV3');
+      const OmniFactoryV3 =
+        await hre.ethers.getContractFactory('OmniFactoryV3');
       const factoryContract = await OmniFactoryV3.deploy(lzEndpointAddress);
 
       // ---------- act ---------- //
@@ -19,22 +21,24 @@ describe('OmniFactoryV3',  () => {
 
       // ---------- assert ---------- //
       expect(actual).to.equal(lzEndpointAddress);
-    })
-  })
+    });
+  });
 
-  describe('deployLocalCoin()',  () => {
+  describe('deployLocalCoin()', () => {
     it('should deploy coin on local chain', async () => {
       // ---------- arrange ---------- //
       // prepare the owner
       const [owner] = await hre.ethers.getSigners();
 
       // prepare the endpoints
-      const LZEndpointMock = await hre.ethers.getContractFactory('LZEndpointMock');
+      const LZEndpointMock =
+        await hre.ethers.getContractFactory('LZEndpointMock');
       const lzEndpoint = await LZEndpointMock.deploy(1);
       const lzEndpointAddress = await lzEndpoint.getAddress();
 
       // deploy the factory contract
-      const OmniFactoryV3 = await hre.ethers.getContractFactory('OmniFactoryV3');
+      const OmniFactoryV3 =
+        await hre.ethers.getContractFactory('OmniFactoryV3');
       const factoryContract = await OmniFactoryV3.deploy(lzEndpointAddress);
 
       // prepare the coin details
@@ -47,12 +51,14 @@ describe('OmniFactoryV3',  () => {
 
       // ---------- act ---------- //
       // run deployLocalCoin()
-      await (await factoryContract.deployLocalCoin(
-        coinDetails.name,
-        coinDetails.symbol,
-        coinDetails.decimals,
-        coinDetails.totalSupply,
-      )).wait()
+      await (
+        await factoryContract.deployLocalCoin(
+          coinDetails.name,
+          coinDetails.symbol,
+          coinDetails.decimals,
+          coinDetails.totalSupply,
+        )
+      ).wait();
 
       // retreive the coin address deployed
       const [localCoinDeployedEvent] = await factoryContract.queryFilter(
@@ -73,7 +79,117 @@ describe('OmniFactoryV3',  () => {
       expect(await coinDeployed.name()).to.equal(coinDetails.name);
       expect(await coinDeployed.symbol()).to.equal(coinDetails.symbol);
       expect(await coinDeployed.decimals()).to.equal(coinDetails.decimals);
-      expect(await coinDeployed.totalSupply()).to.equal(coinDetails.totalSupply);
-    })
-  })
-})
+      expect(await coinDeployed.totalSupply()).to.equal(
+        coinDetails.totalSupply,
+      );
+    });
+  });
+  describe('deployRemoteCoin()', () => {
+    it('should deploy coin on remote chain', async () => {
+      // ---------- arrange ---------- //
+      // prepare the chain id
+      const [CHAIN_A, CHAIN_B] = [69, 420];
+
+      // prepare the owner
+      const [owner] = await hre.ethers.getSigners();
+
+      // prepare the endpoints
+      const LZEndpointMock =
+        await hre.ethers.getContractFactory('LZEndpointMock');
+      const lzEndpointA = await LZEndpointMock.deploy(CHAIN_A);
+      const lzEndpointB = await LZEndpointMock.deploy(CHAIN_B);
+      const lzEndpointAddressA = await lzEndpointA.getAddress();
+      const lzEndpointAddressB = await lzEndpointB.getAddress();
+
+      // deploy the factory contract
+      const OmniFactoryV3 =
+        await hre.ethers.getContractFactory('OmniFactoryV3');
+      const factoryContractA = await OmniFactoryV3.deploy(lzEndpointAddressA);
+      const factoryContractB = await OmniFactoryV3.deploy(lzEndpointAddressB);
+      const factoryContractAddressA = await factoryContractA.getAddress();
+      const factoryContractAddressB = await factoryContractB.getAddress();
+
+      // set lzmock endpoint address
+      await lzEndpointA.setDestLzEndpoint(
+        factoryContractAddressB,
+        lzEndpointAddressB,
+      );
+      await lzEndpointB.setDestLzEndpoint(
+        factoryContractAddressA,
+        lzEndpointAddressA,
+      );
+
+      // set trusted network
+      factoryContractA.setTrustedRemote(
+        CHAIN_B,
+        hre.ethers.solidityPacked(
+          ['address', 'address'],
+          [factoryContractAddressB, factoryContractAddressA],
+        ),
+      );
+      factoryContractB.setTrustedRemote(
+        CHAIN_A,
+        hre.ethers.solidityPacked(
+          ['address', 'address'],
+          [factoryContractAddressA, factoryContractAddressB],
+        ),
+      );
+
+      // prepare the coin details
+      const coinDetails = {
+        name: 'Omni Pepe',
+        symbol: 'POPO',
+        decimals: '18',
+        totalSupply: '1000000000000000000000',
+      };
+
+      // ---------- act ---------- //
+      // get fees
+      const [nativeFee] = await factoryContractA.estimateFee(
+        coinDetails.name,
+        coinDetails.symbol,
+        coinDetails.decimals,
+        coinDetails.totalSupply,
+        owner.address,
+        CHAIN_B,
+      );
+
+      // run deployLocalCoin()
+      await (
+        await factoryContractA.deployRemoteCoin(
+          coinDetails.name,
+          coinDetails.symbol,
+          coinDetails.decimals,
+          coinDetails.totalSupply,
+          owner.address,
+          CHAIN_B,
+          factoryContractAddressB,
+          { value: nativeFee },
+        )
+      ).wait();
+
+      // retreive the coin address deployed
+      const [localCoinDeployedEventB] = await factoryContractB.queryFilter(
+        factoryContractB.filters.LocalCoinDeployed(),
+        'latest',
+      );
+      const [coinDeployedAddress, receiver] = localCoinDeployedEventB.args;
+
+      // connect to coin deployed contract
+      const coinDeployed = await hre.ethers.getContractAt(
+        'OmniCoinV3',
+        coinDeployedAddress,
+      );
+
+      // ---------- assert ---------- //
+      expect(coinDeployedAddress).to.not.be.empty;
+      expect(receiver).to.equal(owner.address);
+      expect(await coinDeployed.name()).to.equal(coinDetails.name);
+      expect(await coinDeployed.symbol()).to.equal(coinDetails.symbol);
+      expect(await coinDeployed.decimals()).to.equal(coinDetails.decimals);
+      expect(await coinDeployed.totalSupply()).to.equal(
+        coinDetails.totalSupply,
+      );
+    });
+  });
+});
